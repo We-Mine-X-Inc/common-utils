dd3e54fed66525c020bab8feb2dcecfebf18f0f3
adwemine <108937310+adwemine@users.noreply.github.com>
2023-11-25 14:48:58 -0500

    remove _id from graphql insert request

--- src/databases/mutations/farm-mgmt/insertMinerError.ts
+++ src/databases/mutations/farm-mgmt/insertMinerError.ts
@@ -2,10 +2,11 @@ import { gql } from "@apollo/client";
 import { MinerError } from "wemine-apis";
 
 export function insertMinerError(minerError: MinerError) {
+  const newMinerError = { ...minerError, _id: undefined };
   return gql`
   mutation InsertOneMiningWork {
     insertOneMinererror(
-      data: ${{ ...minerError }}) {
+      data: ${{ ...newMinerError }}) {
         _id
         stackTrace
     }
