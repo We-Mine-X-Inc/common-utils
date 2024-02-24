6f34e4fcc8fe52533de04ea2f5acf45a0f637d4b
adwemine <108937310+adwemine@users.noreply.github.com>
2024-02-24 10:39:45 -0500

    add function to update HostingContract by ID

---
+++ lib/src/databases/mutations/farm-mgmt/updateHostingContractById.d.ts
@@ -0,0 +1,9 @@
+import { Environment } from "wemine-apis";
+import { IdQuery } from "../../queries/id-query";
+import { UpdateDataObject } from "../update-data-obj";
+export declare function updateHostingContractById({ env, query, updatedProperties, }: {
+    env: Environment;
+    query: IdQuery;
+    updatedProperties: UpdateDataObject;
+}): import("@apollo/client").DocumentNode;
+//# sourceMappingURL=updateHostingContractById.d.ts.map
---
+++ src/databases/mutations/farm-mgmt/updateHostingContractById.ts
@@ -0,0 +1,36 @@
+import { gql } from "@apollo/client";
+import { Environment } from "wemine-apis";
+import { getHostingContractGraphSchemaName } from "../../environment-tables";
+import { IdQuery } from "../../queries/id-query";
+import { UpdateDataObject } from "../update-data-obj";
+import { makeGraphQLInputCompatible } from "../../json-manipulation";
+
+export function updateHostingContractById({
+  env,
+  query,
+  updatedProperties,
+}: {
+  env: Environment;
+  query: IdQuery;
+  updatedProperties: UpdateDataObject;
+}) {
+  const schemaName = getHostingContractGraphSchemaName(env, {
+    embeddedInFunction: true,
+  });
+  const compatibleQuery = makeGraphQLInputCompatible(query);
+  const compatibleMutation = makeGraphQLInputCompatible(updatedProperties);
+  return gql`
+  mutation {
+    updateOne${schemaName}(query: ${compatibleQuery}, set: ${compatibleMutation}) {
+      _id
+      minerIntakeStage
+      poolActivity {
+        expectedActivePoolIndex
+      }
+      poolMiningOptions {
+        poolId
+      }
+    }
+  }
+`;
+}
