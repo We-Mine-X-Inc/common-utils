931e2ecfabc24aa0744f88072020a8a033420070
adwemine <108937310+adwemine@users.noreply.github.com>
2024-03-21 17:38:19 -0400

    call correct recursive function to remove nested null and undefine

--- src/jsconstructs/remove-keys.ts
+++ src/jsconstructs/remove-keys.ts
@@ -23,7 +23,7 @@ export function removeNestedNullUndefined(obj: any) {
       if (obj[key] === null || obj[key] === undefined) {
         delete obj[key];
       } else if (typeof obj[key] === "object") {
-        removeNestedNullUndefined(obj[key]);
+        innerRemoveNestedNullUnfedined(obj[key]);
       }
     }
   }
